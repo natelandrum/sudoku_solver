5a26178443502d624906a394e5c78cb52ad6ae1a
natelandrum <nate178dawg@gmail.com>
2025-01-17 14:00:30 -0600

    Add API functionality

--- src/sudoku_solver_app.erl
+++ src/sudoku_solver_app.erl
@@ -10,6 +10,13 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+    {'_', [{"/", sudoku_solver_handler, []}]}
+]),
+{ok, _} = cowboy:start_clear(http_listener,
+    [{port, 8080}],
+    #{env => #{dispatch => Dispatch}}
+),
     sudoku_solver_sup:start_link().
 
 stop(_State) ->
---
+++ src/sudoku_solver_handler.erl
@@ -0,0 +1,45 @@
+-module(sudoku_solver_handler).
+
+-behaviour(cowboy_handler).
+
+-export([init/2]).
+
+-define(CONTENT_TYPE_JSON, #{<<"content-type">> => <<"application/json">>}).
+
+init(Req, State) ->
+    % Read and parse the JSON body
+    case cowboy_req:read_body(Req) of
+        {ok, Body, Req1} ->
+            io:format("Raw Body: ~s~n", [Body]),
+            try
+                ParsedBody = jsx:decode(Body, [return_maps]),
+                io:format("Decoded JSON: ~p~n", [ParsedBody]),
+                case maps:get(<<"grid">>, ParsedBody, undefined) of
+                    SudokuBoard when is_list(SudokuBoard) ->
+                        % Solve the board
+                        io:format("Board: ~p~n", [SudokuBoard]),
+                        case sudoku_solver:solve(SudokuBoard) of
+                            {ok, SolvedBoard} ->
+                                Response = jsx:encode(#{status => <<"success">>, solved_board => SolvedBoard}),
+                                {ok, Req2} = cowboy_req:reply(200, ?CONTENT_TYPE_JSON, Response, Req1),
+                                {ok, Req2, State};
+                            {error, Reason} ->
+                                ErrorResponse = jsx:encode(#{status => <<"error">>, message => Reason}),
+                                {ok, Req2} = cowboy_req:reply(400, ?CONTENT_TYPE_JSON, ErrorResponse, Req1),
+                                {ok, Req2, State}
+                        end;
+                    _ ->
+                        ErrorResponse = jsx:encode(#{status => <<"error">>, message => <<"Invalid Sudoku board structure or missing 'grid' property.">>}),
+                        {ok, Req2} = cowboy_req:reply(400, ?CONTENT_TYPE_JSON, ErrorResponse, Req1),
+                        {ok, Req2, State}
+                end
+            catch
+                _: Error ->
+                    Error
+            end;
+        {error, Reason} ->
+            io:format("Error reading body: ~p~n", [Reason]),
+            ErrorResponse = jsx:encode(#{status => <<"error">>, message => <<"Unable to read request body.">>}),
+            {ok, Req2} = cowboy_req:reply(500, ?CONTENT_TYPE_JSON, ErrorResponse, Req),
+            {ok, Req2, State}
+    end.
